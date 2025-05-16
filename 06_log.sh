745bd928cb755175d8ee242b4065ebb24d736293
raviaws004 <raviaws004@gmail.com>
2025-05-16 09:01:20 +0530

    changes made

--- 06_log.sh
+++ 06_log.sh
@@ -5,21 +5,23 @@ ID=$(id -u)
 TIMESTAMP=$(date "+%F-%T")  # %F = YYYY-MM-DD, %T = HH:MM:SS
 LOGFILE="/tmp/$(basename $0)-$TIMESTAMP.log"
 
-
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 # Function to validate command execution
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo -e "\e[31m❌ FAILED: $2\e[0m"  # Red text
+        echo "❌ ERROR: $2 ... $R FAILED $N"
         exit 1
     else
-        echo -e "\e[32m✅ SUCCESS: $2\e[0m"  # Green text
+        echo "✅ SUCCESS: $2 ... $G SUCESS $N"
     fi
 }
 
 # Check for root access
 if [ $ID -ne 0 ]; then 
-    echo "❌ ERROR: Please run this script with root access"
+    echo "$R ❌ ERROR: Please run this script with root access $N"
     exit 1
 else 
     echo "✅ You are a Root User"
