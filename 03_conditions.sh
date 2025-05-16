be787c252f59acb7fef87bf4d20dab20a546d3e0
raviaws004 <raviaws004@gmail.com>
2025-05-16 06:42:52 +0530

    chnages made

--- 03_conditions.sh
+++ 03_conditions.sh
@@ -4,16 +4,16 @@ ID=$(id -u)
 
 if [ "$ID" -ne 0 ]; then
     echo "Error: Please run the script with root access"
+    exit 1
 else
     echo "You are a Root User"
 fi
 
-yum install mysql
+yum install -y mysql
 
-if [$? -ne 0]
-then 
-    echo "ERROR:: Installing MYSQL is failed"
+if [ $? -ne 0 ]; then
+    echo "ERROR: Installing MYSQL failed"
     exit 1
 else 
-    echo "Installing MYSQL is Success!!!"
+    echo "Installing MYSQL succeeded!"
 fi
