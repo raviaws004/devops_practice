fe47c6d1fd936243bb024e48a6fb1356cad5f3b3
raviaws004 <raviaws004@gmail.com>
2025-05-20 05:52:24 +0530

    changes made

--- 08_packages.sh
+++ 08_packages.sh
@@ -7,15 +7,14 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-# Function to validate command execution
-#VALIDATE() {
+
     if [ $1 -ne 0 ]; then
         echo -e "❌ ERROR: $2 ... $R FAILED $N"
         exit 1
     else
         echo -e "✅ SUCCESS: $2 ... $G SUCCESS $N"
     fi
-#}
+
 
 
 echo "All arguments passed: $@"
