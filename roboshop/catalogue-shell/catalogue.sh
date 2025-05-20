b7963f45ce115389fb72b73a3a285f1ac39b6d5f
raviaws004 <raviaws004@gmail.com>
2025-05-20 08:42:14 +0530

    changes made

---
+++ roboshop/catalogue-shell/catalogue.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date "+%F-%T")  # %F = YYYY-MM-DD, %T = HH:MM:SS
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "script start executing at $TIMESTAMP" &>> $LOGFILE 
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "❌ ERROR: $2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "✅ SUCCESS: $2 ... $G SUCCESS $N"
+    fi
+}
+
+
+ if [ $ID -ne 0 ]; then
+        echo -e "❌ ERROR: $2 ... $R PLease run the script with root access $N"
+        exit 1
+    else
+        echo -e "✅ SUCCESS: $2 ... $G You are a root user $N"
+    fi
+
+
+dnf module disable nodejs -y
+VALIDATE $? "Diabling ... nodejs" &>> $LOGFILE
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling ... nodejs:20" &>> $LOGFILE
+
+dnf install nodejs -y
+VALIDATE $? "Installing ... nodejs" &>> $LOGFILE
+
+useradd roboshop
+VALIDATE $? "Adding User ... roboshop " &>> $LOGFILE
+
+mkdir /app
+VALIDATE $? "Creating ... Application directory " &>> $LOGFILE
+
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+VALIDATE $? "Downloading ... catalogue.zip file from S3 Bucket" &>> $LOGFILE
+
+cd /app 
+VALIDATE $? "Changing directory ... app " &>> $LOGFILE
+
+unzip /tmp/catalogue.zip
+VALIDATE $? "Unzipping catalogue.zip in /tmp directory " &>> $LOGFILE
+
+cd /app 
+VALIDATE $? "Changing directory ... app " &>> $LOGFILE
+
+npm install 
+VALIDATE $? "Installing ... npm package ... dependencies " &>> $LOGFILE
+
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+
